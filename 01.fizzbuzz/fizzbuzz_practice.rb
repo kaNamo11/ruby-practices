8ea587b21212bdd8f55d473f8fda4c9d69538c7e
kaNamo <yumina@kanamoriyuminanoMacBook-Air.local>
2025-06-12 17:06:49 +0900

    fizzbuzz問題を修正(2回目)

--- 01.fizzbuzz/fizzbuzz_practice.rb
+++ 01.fizzbuzz/fizzbuzz_practice.rb
@@ -1,18 +1,17 @@
-number_s = 0
-remainderMod_3 = 0
-multiple_5 = 0
+remainder_mod_3 = 0
+remainder_mod_5 = 0
 
-20.times do
-  number_s += 1
-  remainderMod_3 = number_s % 3
-  multiple_5 = number_s % 5
-  if remainderMod_3 == 0 && multiple_5 == 0
-  	puts "FizzBuzz"
-  elsif remainderMod_3 == 0
-  	puts "Fizz"
-  elsif  multiple_5 == 0
-  	puts "Buzz" 
+20.times{|n|
+  n += 1
+  remainder_mod_3 = n % 3
+  remainder_mod_5 = n % 5
+  if remainder_mod_3 == 0 && remainder_mod_5 == 0
+    puts "FizzBuzz"
+  elsif remainder_mod_3 == 0
+    puts "Fizz"
+  elsif  remainder_mod_5 == 0
+    puts "Buzz" 
   else
-  	puts number_s
-  end
+    puts n
 end
+}
