b76a6642f714258795650f95a6b517e6fd00f1ff
kaNamo <yum.kanamori@gmail.com>
2025-06-25 12:27:42 +0900

    カレンダーの問題を修正

--- 02.calendar/cal.rb
+++ 02.calendar/cal.rb
@@ -1,38 +1,27 @@
 require 'date'
 require 'optparse'
 
-options = {}
+options = {
+  year:  Date.today.year
+  month: Date.today.month
+}
 opt = OptionParser.new
 opt.on("-m", "--month MONTH", Integer, "this_month") { |m| options[:month] = m }
 opt.on("-y", "--year YEAR", Integer, "this_year") { |y| options[:year] = y }
 opt.parse(ARGV)
 
-today = Date.today
+date_start = Date.new(options[:year],options[:month],1)
+date_end = Date.new(options[:year],options[:month],-1)
 
-if options[:year] && options[:month]
-  data_start = Date.new(options[:year],options[:month],1)
-  data_end = Date.new(options[:year],options[:month],-1)
-elsif options[:month]
-  data_start = Date.new(today.year,options[:month],1) 
-  data_end = Date.new(today.year,options[:month],-1)
-elsif options[:year]
-  data_start = Date.new(options[:year],today.month,1)
-  data_end = Date.new(options[:year],today.month,-1)
-else
-  data_start = Date.new(today.year,today.month,1)
-  data_end =  Date.new(today.year,today.month,-1)
-end
-
-puts "#{data_start.month}月#{data_start.year}年".to_s.center(20)
+puts "#{date_start.month}月#{date_start.year}年".to_s.center(20)
 puts "日 月 火 水 木 金 土"
-print "   " * data_start.wday
+print "   " * date_start.wday
 
-loop do
-  if data_start.saturday?
-    puts data_start.day.to_s.rjust(3)
+(data_start..data_end).each do |data|
+  date_view = date_start.day.to_s.rjust(3)
+  if date_start.saturday?
+    puts date_view
   else
-    print data_start.day.to_s.rjust(3)
+    print date_view
   end
-  data_start += 1
-  break if data_start > data_end
 end
