422c1e4036b3d1aca1c735960588b1dfddb26b36
kaNamo <yum.kanamori@gmail.com>
2025-07-22 10:34:04 +0900

    ボウリングの課題を修正(frames[i + 2]の変数名)

--- 03.bowling/bowling.rb
+++ 03.bowling/bowling.rb
@@ -16,16 +16,17 @@ frames = shots.each_slice(2).to_a
 
 point = frames [0..9].each_with_index.sum do |frame, i|
   next_frame = frames[i + 1]
+  next_2_frame = frames[i + 2]
   point = if i == 9
-            if frames[i + 2]
-              frame.sum + next_frame[0] + frames[i + 2][0]
+            if next_2_frame
+              frame.sum + next_frame[0] + next_2_frame[0]
             elsif next_frame
               frame.sum + next_frame.sum
             end
           elsif i < 9
             if frame[0] == 10
-              if next_frame[0] == 10 && frames[i + 2][0]
-                frame.sum + next_frame[0] + frames[i + 2][0]
+              if next_frame[0] == 10 && next_2_frame[0]
+                frame.sum + next_frame[0] + next_2_frame[0]
               elsif next_frame
                 frame.sum + next_frame[0..1].sum
               end
