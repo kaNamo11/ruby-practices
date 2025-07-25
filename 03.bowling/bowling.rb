8662a621d9047298c826fabbdbbc078c9121364b
kaNamo <yum.kanamori@gmail.com>
2025-07-25 15:24:48 +0900

    ボウリングの課題を修正(if文)

--- 03.bowling/bowling.rb
+++ 03.bowling/bowling.rb
@@ -14,34 +14,24 @@ end
 
 frames = shots.each_slice(2).to_a
 
-point = frames [0..9].each_with_index.sum do |frame, i|
+point = frames[0..9].each_with_index.sum do |frame, i|
   next_frame = frames[i + 1]
-  next_2_frame = frames[i + 2]
-  point = if i == 9
-            if next_2_frame
-              frame.sum + next_frame[0] + next_2_frame[0]
-            elsif next_frame
-              frame.sum + next_frame.sum
-            else
-              frame.sum
-            end
-          elsif i < 9
-            if frame[0] == 10
-              if next_frame[0] == 10 && next_2_frame[0]
-                frame.sum + next_frame[0] + next_2_frame[0]
-              elsif next_frame
-                frame.sum + next_frame[0..1].sum
-              else
-                frame.sum
-              end
-            elsif next_frame && frame.sum == 10
-              frame.sum + next_frame[0]
-            else
-              frame.sum
-            end
-          else
-            frame.sum
-          end
+  after_next_frame = frames[i + 2]
+  if i == 9
+    [frame, next_frame, after_next_frame].compact.flatten.sum
+  elsif i < 9
+    if frame[0] == 10
+      if next_frame[0] == 10 && after_next_frame[0]
+        frame.sum + next_frame[0] + after_next_frame[0]
+      elsif next_frame
+        frame.sum + next_frame[0..1].sum
+      end
+    elsif frame.sum == 10
+      frame.sum + next_frame[0]
+    else
+      frame.sum
+    end
+  end
 end
 
 puts point
