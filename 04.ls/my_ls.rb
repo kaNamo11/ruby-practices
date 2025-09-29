53ce9b01779eb0049002d4bc5e64d2a3c3ed008e
kaNamo <yum.kanamori@gmail.com>
2025-09-29 14:43:54 +0900

    1回目の課題修正

--- 04.ls/my_ls.rb
+++ 04.ls/my_ls.rb
@@ -1,23 +1,45 @@
-def make_file_array(number_of_lines)
-  files = Dir.glob('*').sort
+# frozen_string_literal: true
 
-  col = (files.count.to_f / number_of_lines).ceil
-  add_files = number_of_lines * col - files.count
+class MyLs
+  DEFAULT_LIENS = 3
+  CHARCTOR_WIDTH = 20
 
-  add_files.times do |_i|
-    files << nil
+  def mix_characters_ljust(width)
+    files = Dir.glob('*').sort
+    new_files = []
+    files.map do |file|
+      output_width = file.each_char.map { |c| c.bytesize == 1 ? 1 : 2 }.reduce(0, &:+)
+      padding_size = [0, width - output_width].max
+      new_files << file + ' ' * padding_size
+    end
+    new_files
   end
-  [files, col]
+
+  def fetch_file_names(number_of_lines)
+    files = mix_characters_ljust(CHARCTOR_WIDTH)
+    column = (files.count.to_f / number_of_lines).ceil
+    files.count
+
+    files.fill(nil, files.length...(number_of_lines * column))
+
+    [files, column]
+  end
+
+  def main
+    files, column = fetch_file_names(DEFAULT_LIENS)
+
+    if files.empty?
+      puts ''
+    else
+      rows = files.each_slice(column).to_a
+      result = rows.transpose
+
+      result.each do |row|
+        puts row.join(' ')
+      end
+    end
+  end
 end
 
-def display_list
-  files, col = make_file_array(3)
-  rows = files.each_slice(col).to_a # => 3 行（各行 col 要素）
-  result = rows.transpose # => col 行 × 3 列（縦並び）
-
-  result.each do |row|
-    puts row.compact.join(' ')
-  end
-end
-
-display_list
+my_ls = MyLs.new
+my_ls.main
