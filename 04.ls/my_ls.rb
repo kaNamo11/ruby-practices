9dd505a04722b3d542d9ac76fdb59be51ee7d107
kaNamo <yum.kanamori@gmail.com>
2025-11-26 13:05:48 +0900

    rオプションが使用された際はfilenamesに対して.reverseするように変更

--- 04.ls/my_ls.rb
+++ 04.ls/my_ls.rb
@@ -12,7 +12,9 @@ def main
   opt.parse!(ARGV)
 
   flags = params[:a] ? File::FNM_DOTMATCH : 0
-  filenames_matrix = params[:r] ? pad_filenames(Dir.glob('*', flags)).reverse : pad_filenames(Dir.glob('*', flags))
+  filenames = Dir.glob('*', flags)
+  filenames.reverse! if params[:r]
+  filenames_matrix = pad_filenames(filenames) 
   formatted_file_names = convert_filenames_to_matrix(filenames_matrix, DEFAULT_LINES)
   print_file_names(formatted_file_names[0], formatted_file_names[1])
 end
